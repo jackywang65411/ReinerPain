529fe27db2fa538e880734637074efd153599faf
rStar <starlove124760@gmail.com>
2021-04-11 00:13:43 +0800

    feat: spawn raina in random position of scene

--- Assets/Scripts/GameCore/GameMechanics/SpawnManager.cs
+++ Assets/Scripts/GameCore/GameMechanics/SpawnManager.cs
@@ -1,15 +1,34 @@
+using System;
+using UniRx;
 using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
 
 namespace GameCore.GameMechanics
 {
     public class SpawnManager : MonoBehaviour
     {
         [SerializeField]
-        private GameObject spawnObject;
+        [Header("將被產生的物件")]
+        private GameObject prefab;
+
+        [SerializeField]
+        private float eachSpawnTime;
 
         private void Start()
         {
+            var spawnTime = TimeSpan.FromSeconds(eachSpawnTime);
+            Observable.Timer(spawnTime , spawnTime)
+                      .Subscribe(_ => SpawnObject())
+                      .AddTo(gameObject);
+        }
 
+        private void SpawnObject()
+        {
+            Instantiate(prefab
+                , Random.insideUnitCircle
+                , Quaternion.identity
+                , transform);
         }
     }
 }
