bf8717e01ec13217ac7bf1aae6fdd316e56f30ee
Rino <sherikawaii3390@gmail.com>
2021-04-10 17:31:08 +0800

    開頭跟離開加上程式ˋˇˊ

---
+++ Assets/Scripts/Quit/QuitScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitScene : MonoBehaviour {
+	// Start is called before the first frame update
+	void Start() {
+		Invoke("Quit", 8f);
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+}
