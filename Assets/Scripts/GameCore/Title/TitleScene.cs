38ee326f25bc7585885f1c1c67bd027aa36a27a9
xueru <toup1314@mail.fcu.edu.tw>
2021-04-11 15:45:13 +0800

    add title scene

--- Assets/Scripts/GameCore/Title/TitleScene.cs
+++ Assets/Scripts/GameCore/Title/TitleScene.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleScene : MonoBehaviour
 {
+	public Button btnStart;
+	public GameObject objCutin;
+	public AudioSource audbgm;
+    private void Start() {
+		btnStart.interactable = true;
+		objCutin.SetActive(false);
+
+	}
     public void QuitGame() {
-		SceneManager.LoadScene("Quit");
+		btnStart.interactable = false;
+		StartCoroutine(StartGame());
+	}
+	IEnumerator StartGame() {
+		audbgm.Stop();
+		objCutin.SetActive(true);
+		AudioManagerScript.Instance.PlayAudioClip("reiner_seat_down");
+		yield return new WaitForSeconds(1.8f);
+		SceneManager.LoadScene("GameScene");
 	}
 }
