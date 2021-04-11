72ee2bb13e8d55936387725f2ba1b474de0f4c44
NK.Shien <shien091091@gmail.com>
2021-04-11 13:14:25 +0800

    Ending Scene

---
+++ Assets/Scripts/Ending/Manager/EndingManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndingManager : MonoBehaviour
+{
+    private static EndingManager _instance;
+    public static EndingManager Instance { get { return _instance; } }
+
+    [SerializeField] private Slider sld;
+    [SerializeField] private float sliderValue;
+    [SerializeField] private Animator anim;
+    [SerializeField] private bool updatingSlider;
+    [SerializeField] private int scoreTop;
+
+    void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+
+        }
+
+        if (updatingSlider)
+        {
+            UpdateSliderValue();
+        }
+    }
+
+    /// <summary>
+    /// 顯示結束畫面
+    /// </summary>
+    /// <param name="score">分數</param>
+    public void ShowEnding(int score)
+    {
+        if (anim == null) anim = this.GetComponent<Animator>();
+
+        EndingProccess(score, () =>
+        {
+            anim.SetTrigger("playEnding");
+        });
+
+    }
+
+    private void EndingProccess(int score, System.Action body)
+    {
+        if (sld == null) sld = this.GetComponentInChildren<Slider>();
+
+        scoreTop = score;
+        updatingSlider = true;
+
+        body.Invoke();
+
+        //scoreTop = 0;
+        //updatingSlider = false;
+    }
+
+    private void UpdateSliderValue()
+    {
+        sld.value = sliderValue * ( scoreTop / sld.maxValue );
+    }
+}
---
+++ Assets/Scripts/Ending/ScorePanel/LineCreator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineCreator : MonoBehaviour
+{
+    public List<int> scoreStandard;
+    public GameObject linePrefab;
+
+    [ContextMenu("CreateLines")]
+    public void CreateLines()
+    {
+        if (scoreStandard == null || scoreStandard.Count <= 0 || linePrefab == null)
+            return;
+
+        RectTransform _rt = this.GetComponent<RectTransform>();
+
+        if (_rt == null)
+            return;
+
+        Slider _sld = GetComponentInParent<Slider>();
+        int _maxValue = _sld != null ? (int)_sld.maxValue : 0;
+        float _unit = _rt.rect.height / _maxValue;
+
+        for (int i = 0; i < scoreStandard.Count; i++)
+        {
+            GameObject _go = Instantiate(linePrefab, this.transform);
+            Text _scoreTxt = _go.GetComponentInChildren<Text>();
+            _scoreTxt.text = scoreStandard[i].ToString();
+            _go.transform.localPosition = new Vector3(transform.localPosition.x, ( -_rt.rect.height / 2 ) + ( scoreStandard[i] * _unit ), transform.localPosition.z);
+        }
+
+
+    }
+}
